966b7ce6ab868c479d1b7cbf22574ec794efabed
LINZHIBING <feimi333@outlook.com>
2026-05-13 23:03:42 +0800

    Initial Commit

---
+++ CaloriePet/CaloriePetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriePetApp.swift
+//  CaloriePet
+//
+//  Created by 冷极冰 on 2026/5/13.
+//
+
+import SwiftUI
+
+@main
+struct CaloriePetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
